500283f16bcb2a907e636f5f647fb6d51665c0dd
RohanSharma-exe <68742910+RohanSharma-exe@users.noreply.github.com>
2026-09-23 11:49:02 +0000

    Solve: Vector Element-wise Sum

---
+++ problems/0121-vector-element-wise-sum/solution.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def vector_sum(a: list[int|float], b: list[int|float]) -> list[int|float]:
+	# Return the element-wise sum of vectors 'a' and 'b'.
+	# If vectors have different lengths, return -1.
+	if len(a) != len(b):
+		return -1
+	
+	a = np.array(a)
+	b = np.array(b)
+
+	return a + b
